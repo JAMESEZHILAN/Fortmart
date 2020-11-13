55d3236313fef04f3ea968b10f0f541c787991a9
JAMESEZHILAN <jamz07eziln96@gmail.com>
2020-11-13 14:50:01 +0530

    Add; added searchView in categoryList screen

--- app/src/main/java/com/fortmart/customer/ui/dashboard/stores/storeList/categoryList/CategoryListFragment.kt
+++ app/src/main/java/com/fortmart/customer/ui/dashboard/stores/storeList/categoryList/CategoryListFragment.kt
@@ -1,9 +1,14 @@
 package com.fortmart.customer.ui.dashboard.stores.storeList.categoryList
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -28,12 +33,25 @@ class CategoryListFragment : BaseFragment(R.layout.fragment_category_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCategoryListBinding.bind(view)
         rvAdapter = CategoryRVAdapter(
             arrayOf(
+                ImageMenu("Fruits & vegitebles", R.drawable.ic_fruits_1_1_),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.ic_fruits_1_1_),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.ic_fruits_1_1_),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
+                ImageMenu("Fruits & vegitebles", R.drawable.fruits),
                 ImageMenu("Fruits & vegitebles", R.drawable.fruits)
             )
         )
@@ -44,10 +62,34 @@ class CategoryListFragment : BaseFragment(R.layout.fragment_category_list) {
             layoutManager = GridLayoutManager(requireContext(), 3)
             adapter = rvAdapter!!
         }
+        setUpSearchView(binding?.searchView!!)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         activity?.menuInflater?.inflate(R.menu.menu_address, menu)
+    }
+
+    private fun setUpSearchView(searchView: SearchView){
+        searchView.setOnCloseListener {
+            true
+        }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrEmpty()) {
+//                    refresh(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+//                refresh(newText ?: "")
+                return false
+            }
+        })
+
+        val searchManager =
+            requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
     }
 
 }
